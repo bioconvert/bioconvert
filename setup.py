b72e91f730eb8c593ceb7d26570ed97702113f07
Bertrand Néron <bneron@pasteur.fr>
2017-10-14 00:51:24 +0200

    setup.py cleaning
    remove unused import
    fix
    - classifiers
    - authors
    
    TODO
     fix license, platforms, maintainers, ...

--- setup.py
+++ setup.py
@@ -1,86 +1,74 @@
 # -*- coding: utf-8 -*-
-import sys
-import os
-import glob
+
 from setuptools import setup, find_packages
 
-
-_MAJOR               = 0
-_MINOR               = 0
-_MICRO               = 1
-version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
-release              = '%d.%d' % (_MAJOR, _MINOR)
+_MAJOR = 0
+_MINOR = 0
+_MICRO = 1
+version = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+release = '%d.%d' % (_MAJOR, _MINOR)
 
 metainfo = {
     'authors': {
-        'Cokelaer':('Thomas Cokelaer','thomas.cokelaer@pasteur.fr'),
+        'Cokelaer': ('Thomas Cokelaer', 'thomas.cokelaer@pasteur.fr'),
         },
     'version': version,
-    'license' : 'BSD',
-    'download_url' : ['http://pypi.python.org/pypi/bioconvert'],
-    'url' : ['http://pypi.python.org/pypi/bioconvert'],
-    'description':'Access to Biological Web Services from Python' ,
-    'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
+    'license': 'BSD',
+    'download_url': ['http://pypi.python.org/pypi/bioconvert'],
+    'url': ['http://pypi.python.org/pypi/bioconvert'],
+    'description': 'convert between bioinformatics formats',
+    'platforms': ['Linux', 'Unix', 'MacOsX', 'Windows'],
     "keywords": ["NGS", "bam2bed", "fastq2fasta"],
-    'classifiers' : [
+    'classifiers': [
           'Development Status :: 1 - Planning',
           'Intended Audience :: Developers',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: BSD License',
           'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Topic :: Software Development :: Libraries :: Python Modules',
           'Topic :: Scientific/Engineering :: Bio-Informatics',
-          'Topic :: Scientific/Engineering :: Information Analysis',
-          'Topic :: Scientific/Engineering :: Mathematics',
-          'Topic :: Scientific/Engineering :: Physics']
+          'Topic :: Scientific/Engineering :: Information Analysis',]
     }
+
 
 with open('README.rst') as f:
     readme = f.read()
 
-#from distutils.core import setup, Extension
-#sequence = Extension('sequence', sources=['biokit/sequence/sequence.c'])
-
 setup(
-    name             = 'bioconvert',
-    version          = version,
-    maintainer       = metainfo['authors']['Cokelaer'][0],
-    maintainer_email = metainfo['authors']['Cokelaer'][1],
-    author           = metainfo['authors']['Cokelaer'][0],
-    author_email     = metainfo['authors']['Cokelaer'][1],
-    long_description = readme,
-    keywords         = metainfo['keywords'],
-    description      = metainfo['description'],
-    license          = metainfo['license'],
-    platforms        = metainfo['platforms'],
-    url              = metainfo['url'],
-    download_url     = metainfo['download_url'],
-    classifiers      = metainfo['classifiers'],
-
+    name='bioconvert',
+    version=version,
+    maintainer=metainfo['authors']['Cokelaer'][0],
+    maintainer_email=metainfo['authors']['Cokelaer'][1],
+    author='The bioconvert Contributors',
+    author_email=metainfo['authors']['Cokelaer'][1],
+    long_description=readme,
+    keywords=metainfo['keywords'],
+    description=metainfo['description'],
+    license=metainfo['license'],
+    platforms=metainfo['platforms'],
+    url=metainfo['url'],
+    download_url=metainfo['download_url'],
+    classifiers=metainfo['classifiers'],
     zip_safe=False,
-    packages = find_packages(),
-    install_requires = open('requirements.txt').read().split(),
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().split(),
 
-     # This is recursive include of data files
-    exclude_package_data = {"": ["__pycache__"]},
+    # This is recursive include of data files
+    exclude_package_data={"": ["__pycache__"]},
 
-    package_data = {
+    package_data={
         '': ['*.csv'],
-        'bioconvert.data' : ['*'],
+        'bioconvert.data': ['*'],
         },
 
-    entry_points = {
-        'console_scripts':[
-           'converter=bioconvert.scripts.bioconvert:main'
+    entry_points={
+        'console_scripts': [
+           'bioconvert=bioconvert.scripts.converter:main'
         ]
     }
-
-#    ext_modules=[
-#        Extension('biokit.sequence.complement', 
-#                sources=['biokit/sequence/cpp/complement.c', ],
-#                 )
-#        
-#        ],
 
     )
