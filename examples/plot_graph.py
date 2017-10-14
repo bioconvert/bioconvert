247dfbf94a7128255df33b16f5caa97fd930017c
Thomas Cokelaer <cokelaer@gmail.com>
2017-10-14 15:54:19 +0200

    fixing RTD image in the gallery

--- examples/plot_graph.py
+++ examples/plot_graph.py
@@ -13,12 +13,13 @@ from bioconvert.core.graph import create_graph
 
 #####################################################
 # Get a data set (BAM file) for testing
-create_graph("conversion", format="png")
+create_graph("conversion", format="svg")
 
-####################################################
-# 
+#####################################################
+#  PNG does not work on RTD with graphviz from conda
+#  SVG cannot be shown 
 from pylab import imshow, imread, xticks, yticks, gca
-imshow(imread("conversion.png"))
+imshow(imread("conversion.svg"))
 xticks([])
 yticks([])
 ax = gca()
