bdc93e73dd3d5d3d66f9128fec27030424cf38af
Thomas Cokelaer <cokelaer@gmail.com>
2017-10-14 16:03:08 +0200

    switch png to eps

--- examples/plot_graph.py
+++ examples/plot_graph.py
@@ -13,13 +13,13 @@ from bioconvert.core.graph import create_graph
 
 #####################################################
 # Get a data set (BAM file) for testing
-create_graph("conversion", format="svg")
+create_graph("conversion", format="eps")
 
 #####################################################
 #  PNG does not work on RTD with graphviz from conda
 #  SVG cannot be shown 
 from pylab import imshow, imread, xticks, yticks, gca
-imshow(imread("conversion.svg"))
+imshow(imread("conversion.eps"))
 xticks([])
 yticks([])
 ax = gca()
