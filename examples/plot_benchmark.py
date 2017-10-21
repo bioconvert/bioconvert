86ef9b0deee2fbec116961db5c4bae0bf5c8abc0
cokelaer <cokelaer@gmail.com>
2017-10-21 18:23:55 +0200

    add new example for the gallery

---
+++ examples/plot_benchmark.py
@@ -0,0 +1,31 @@
+"""
+Converter benchmarking
+===========================
+
+Converter have a default method.
+
+Notem however, that several methods may be available.
+Moreover, you may have a method that you want to compare
+with the implemented one. To do so you will need to 
+implement your method first. Then, simply use our benchmarking
+framework as follows.
+
+"""
+#################################################
+#
+from bioconvert import Benchmark
+from bioconvert import bioconvert_data
+from bioconvert.bam2bed import Bam2Bed
+
+#####################################################
+# Get the convert you wish to benchmark
+input_file = bioconvert_data("test_measles.sorted.bam")
+conv = Bam2Bed(input_file, "test.bed")
+
+#####################################################
+# Get the Benchmark instance
+bench = Benchmark(conv)
+bench.plot()
+
+# You can now see the different methods implemented in this
+# converter and which one is the fastest.
