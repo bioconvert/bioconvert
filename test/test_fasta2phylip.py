7ba986d30c4d98a72236e70670551b0abb8a1938
Anna Zhukova <zhutchok@gmail.com>
2017-10-24 17:18:16 +0200

    phylip2fasta (Issue #40)

--- test/test_fasta2phylip.py
+++ test/test_fasta2phylip.py
@@ -3,18 +3,30 @@ import pytest
 from easydev import TempFile, md5
 
 from bioconvert import bioconvert_data
-from fasta2phylip import FASTA2PHYLIP
+from fasta2phylip import FASTA2PHYLIP, PHYLIP2FASTA
 
 skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ
                                 and os.environ['TRAVIS_PYTHON_VERSION'].startswith("2"), reason="On travis")
 
 
 @skiptravis
-def test_conv():
+def test_conv_fa2phy():
     infile = bioconvert_data("fa2phy.fasta")
     outfile = bioconvert_data("fa2phy_desired_output.phylip")
     with TempFile(suffix=".phylip") as tempfile:
         convert = FASTA2PHYLIP(infile, tempfile.name)
+        convert()
+
+        # Check that the output is correct with a checksum
+        assert md5(tempfile.name) == md5(outfile)
+
+
+@skiptravis
+def test_conv_phy2fa():
+    infile = bioconvert_data("fa2phy_desired_output.phylip")
+    outfile = bioconvert_data("fa2phy.fasta")
+    with TempFile(suffix=".fasta") as tempfile:
+        convert = PHYLIP2FASTA(infile, tempfile.name)
         convert()
 
         # Check that the output is correct with a checksum
