12225df6fe4857e7dc35c5717691db1a4707c55e
Thomas Cokelaer <cokelaer@gmail.com>
2017-10-25 15:00:00 +0200

    update example with a better resolution for the digraph

--- examples/plot_graph.py
+++ examples/plot_graph.py
@@ -13,13 +13,15 @@ from bioconvert.core.graph import create_graph
 
 #####################################################
 # Get a data set (BAM file) for testing
+import matplotlib as mpl
+mpl.rcParams['figure.dpi']= 200
 create_graph("conversion.png")
 
 #####################################################
 #  PNG does not work on RTD with graphviz from conda
 #  SVG cannot be shown 
 from pylab import imshow, imread, xticks, yticks, gca
-imshow(imread("conversion.png"))
+imshow(imread("conversion.png"), interpolation="nearest")
 xticks([])
 yticks([])
 ax = gca()
