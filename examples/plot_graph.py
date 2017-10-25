17ec19e7a312de36f3f66a39175c954578f6b8e8
Thomas Cokelaer <cokelaer@gmail.com>
2017-10-25 10:16:14 +0200

    update example to use new API

--- examples/plot_graph.py
+++ examples/plot_graph.py
@@ -13,13 +13,13 @@ from bioconvert.core.graph import create_graph
 
 #####################################################
 # Get a data set (BAM file) for testing
-create_graph("conversion", format="eps")
+create_graph("conversion", format="png")
 
 #####################################################
 #  PNG does not work on RTD with graphviz from conda
 #  SVG cannot be shown 
 from pylab import imshow, imread, xticks, yticks, gca
-imshow(imread("conversion.eps"))
+imshow(imread("conversion.png"))
 xticks([])
 yticks([])
 ax = gca()
