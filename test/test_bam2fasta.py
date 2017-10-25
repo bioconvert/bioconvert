ec11faa277417790d20028e1abaf784f42e5fd11
khillion <hillion.kenzo@gmail.com>
2017-10-25 15:10:45 +0200

    migration for #17 and addition of methods

---
+++ test/test_bam2fasta.py
@@ -0,0 +1,15 @@
+from bioconvert.bam2fastq import BAM2Fasta
+from bioconvert import bioconvert_data
+from easydev import TempFile, md5
+
+def test_conv():
+    infile = bioconvert_data("test_measles.sorted.bam")
+    with TempFile(suffix=".fa") as tempfile:
+        convert = BAM2Fasta(infile, tempfile.name)
+        convert()
+
+        # Check that the output is correct with a checksum
+        # Note that we cannot test the md5 on a gzip file but only 
+        # on the original data. This check sum was computed
+        # fro the unzipped version of biokit/data/converters/measles.bed
+        assert md5(tempfile.name) == "ea5511c3c8913626be152609887c8c4d"
---
+++ test/test_bam2fastq.py
@@ -0,0 +1,15 @@
+from bioconvert.bam2fastq import BAM2Fastq
+from bioconvert import bioconvert_data
+from easydev import TempFile, md5
+
+def test_conv():
+    infile = bioconvert_data("test_measles.sorted.bam")
+    with TempFile(suffix=".fq") as tempfile:
+        convert = BAM2Fastq(infile, tempfile.name)
+        convert()
+
+        # Check that the output is correct with a checksum
+        # Note that we cannot test the md5 on a gzip file but only 
+        # on the original data. This check sum was computed
+        # fro the unzipped version of biokit/data/converters/measles.bed
+        assert md5(tempfile.name) == "8683ad696e52e4af67670d2631af6d1f"
