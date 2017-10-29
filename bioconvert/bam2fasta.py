f8fec71a0556797cd1e321d939f3e8d7b7194bd9
cokelaer <cokelaer@gmail.com>
2017-10-29 10:45:33 +0100

    add sambamba method

--- bioconvert/bam2fasta.py
+++ bioconvert/bam2fasta.py
@@ -49,3 +49,9 @@ class BAM2Fasta(ConvBase):
         """
         cmd = "samtools fasta {} > {}".format(self.infile, self.outfile)
         self.execute(cmd)
+
+    def _method_sambamba(self, *args, **kwargs):
+        cmd = "sambamba view {} -o {}".format(self.infile, self.outfile)
+        self.execute(cmd)
+
+
