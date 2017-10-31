5805df670c0a2f62d60c88497376a7c57886191e
Thomas Cokelaer <cokelaer@gmail.com>
2017-10-31 10:12:36 +0100

    Fix vcf2bcf and add bcf2vcf

--- bioconvert/bcf2vcf.py
+++ bioconvert/bcf2vcf.py
@@ -1,13 +1,13 @@
-"""Convert :term:`VCF` file to :term:`BCF` file"""
+"""Convert :term:`BCF` file to :term:`VCF` file"""
 from bioconvert import ConvBase
 
 
-class VCF2BCF(ConvBase):
+class BCF2VCF(ConvBase):
     """
 
     """
-    input_ext = [".vcf"]
-    output_ext = [".bcf"]
+    input_ext = [".bcf"]
+    output_ext = [".vcf"]
 
     def __init__(self, infile, outfile, *args, **kargs):
         """.. rubric:: constructor
@@ -17,19 +17,16 @@ class VCF2BCF(ConvBase):
 
         command used::
 
-            bcftools view -Sb
+            bcftools view 
         """
-        super(VCF2BCF, self).__init__(infile, outfile, *args, **kargs)
+        super(BCF2VCF, self).__init__(infile, outfile, *args, **kargs)
 
     def _method_bcftools(self, *args, **kwargs):
-        # -S means ignored (input format is VCF)
-        # -b output BCF instead of VCF
-        #cmd = "bcftools view -Sb {} >  {}".format(self.infile, self.outfile)
 
         # -O, --output-type b|u|z|v Output compressed BCF (b), uncompressed BCF
         # (u), compressed VCF (z), uncompressed VCF (v). Use the -Ou option when
         # piping between bcftools subcommands to speed up performance
-        cmd = "bcftools view {} -O b -o {}".format(self.infile, self.outfile)
+        cmd = "bcftools view {} -O v -o {}".format(self.infile, self.outfile)
         self.execute(cmd)
 
 
