3f3b9cf159b371c980d9f3a069ad8d5bedd6aa7f
Bertrand Néron <bneron@pasteur.fr>
2017-11-03 11:23:44 +0100

    add methods based on squizz tool

--- bioconvert/fasta2phylip.py
+++ bioconvert/fasta2phylip.py
@@ -38,10 +38,22 @@ class FASTA2PHYLIP(ConvBase):
         self.alphabet = alphabet
         self._default_method = 'biopython'
 
-    def _method_biopython(self):
+    def _method_biopython(self, threads=None):
         sequences = list(SeqIO.parse(self.infile, "fasta", alphabet=self.alphabet))
         count = SeqIO.write(sequences, self.outfile, "phylip")
         _log.debug("Converted %d records to phylip" % count)
+
+
+    def _method_squizz(self, threads=None):
+        """
+        Convert fasta file in Phylip interleaved format using squizz tool.
+        The fasta file must be an alignemnt file, yhis mean all the sequences must
+        have the same length (with the gap) otherwise an error will be raised
+        """
+        cmd = 'squizz -c PHYLIPI {infile} > {outfile}'.format(
+            infile=self.infile,
+            outfile=self.outfile)
+        self.execute(cmd)
 
 
 class PHYLIP2FASTA(ConvBase):
@@ -65,9 +77,19 @@ class PHYLIP2FASTA(ConvBase):
         super().__init__(infile, outfile)
         self.alphabet = alphabet
         self._default_method = 'biopython'
-        
-    def _method_biopython(self):
+
+    def _method_biopython(self, threads=None):
         sequences = list(SeqIO.parse(self.infile, "phylip", alphabet=self.alphabet))
         count = SeqIO.write(sequences, self.outfile, "fasta")
         _log.debug("Converted %d records to fasta" % count)
 
+
+    def _method_squizz(self, threads=None):
+        """
+        Convert Phylip inteleaved file in fasta format using squizz tool.
+        The fasta file is an alignemnt, that means the gap are conserved.
+        """
+        cmd = 'squizz -c FASTA {infile} > {outfile}'.format(
+            infile=self.infile,
+            outfile=self.outfile)
+        self.execute(cmd)
