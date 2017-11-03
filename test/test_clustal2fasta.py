c159f7e13dff34f0e94fd74d9b8da8c90aa0b189
Bertrand Néron <bneron@pasteur.fr>
2017-11-03 15:43:52 +0100

    add converter for fasta <=> clustal

---
+++ test/test_clustal2fasta.py
@@ -0,0 +1,56 @@
+import os
+import pytest
+from easydev import TempFile, md5
+
+from bioconvert import bioconvert_data
+from bioconvert.clustal2fasta import CLUSTAL2FASTA, FASTA2CLUSTAL
+
+skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ
+                                and os.environ['TRAVIS_PYTHON_VERSION'].startswith("2"), reason="On travis")
+
+
+@skiptravis
+def test_clustal2fasta_biopython():
+    infile = bioconvert_data("biopython.clustal")
+    outfile = bioconvert_data("biopython.fasta")
+    with TempFile(suffix=".fasta") as tempfile:
+        converter = CLUSTAL2FASTA(infile, tempfile.name)
+        converter(method='biopython')
+
+        # Check that the output is correct with a checksum
+        assert md5(tempfile.name) == md5(outfile)
+
+
+@skiptravis
+def test_fasta2clustal_biopython():
+    infile = bioconvert_data("biopython.fasta")
+    outfile = bioconvert_data("biopython.clustal")
+    with TempFile(suffix=".clustal") as tempfile:
+        converter = FASTA2CLUSTAL(infile, tempfile.name)
+        converter(method='biopython')
+
+        # Check that the output is correct with a checksum
+        assert md5(tempfile.name) == md5(outfile)
+
+@skiptravis
+def test_clustal2fasta_squizz():
+    infile = bioconvert_data("squizz.clustal")
+    outfile = bioconvert_data("squizz.fasta")
+    with TempFile(suffix=".fasta") as tempfile:
+        converter = CLUSTAL2FASTA(infile, tempfile.name)
+        converter(method='squizz')
+
+        # Check that the output is correct with a checksum
+        assert md5(tempfile.name) == md5(outfile)
+
+
+@skiptravis
+def test_fasta2clustal_squizz():
+    infile = bioconvert_data("squizz.fasta")
+    outfile = bioconvert_data("squizz.clustal")
+    with TempFile(suffix=".clustal") as tempfile:
+        converter = FASTA2CLUSTAL(infile, tempfile.name)
+        converter(method='squizz')
+
+        # Check that the output is correct with a checksum
+        assert md5(tempfile.name) == md5(outfile)
