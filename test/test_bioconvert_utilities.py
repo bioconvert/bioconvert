9d185f51d7cc1cd6ac4af1c0666bafdb41f9087f
Bertrand Néron <bneron@pasteur.fr>
2017-11-04 00:06:32 +0100

    remove ugly print used to debug travis-ci

--- test/test_bioconvert_utilities.py
+++ test/test_bioconvert_utilities.py
@@ -5,20 +5,11 @@ from bioconvert import generate_outfile_name, bioconvert_data
 
 
 def test_bioconvert_data():
-    import sys
     file_name = 'squizz.phylip'
-    print("####### interpreter", sys.executable, file=sys.stderr)
-    print("####### bioconvert_data(file_name)", bioconvert_data(file_name), file=sys.stderr)
-    print("####### bioconvert.__path__[0]    ", os.path.join(bioconvert.__path__[0], 'data', file_name), file=sys.stderr)
-    print("####### sys.path                  \n",
-          '\n'.join(['#######                           {}'.format(p) for p in sys.path]), sep='')
-    sys.stderr.flush()
-
     assert bioconvert_data(file_name) == os.path.join(bioconvert.__path__[0], 'data', file_name)
     with pytest.raises(FileNotFoundError, message="Excepting FileNotFoundError"):
         file_name = 'foo.bar'
         assert bioconvert_data(file_name) == os.path.join(bioconvert.__path__[0], 'data', file_name)
-    raise Exception()
 
 
 def test_generate_outfile_name():
