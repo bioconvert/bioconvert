930423784a48b112d74dff8fe7f6b36d9bab115e
Bertrand Néron <bneron@pasteur.fr>
2017-11-04 01:11:12 +0100

    add test to registry (test membership and setitem)

--- test/test_registry.py
+++ test/test_registry.py
@@ -1,4 +1,4 @@
-
+import pytest
 from bioconvert.core.registry import Registry
 
 
@@ -7,3 +7,11 @@ def test_registry():
     rr = Registry()
     assert rr.conversion_exists("bam", "bed") is True
     assert rr.conversion_exists("bam", "dummy") is False
+
+    assert ('.bam', '.bed') in rr
+    assert ('.nimport', '.naoik') not in rr
+
+    converter = rr[('.bam', '.bed')]
+    with pytest.raises(KeyError) as err:
+        rr[('.bam', '.bed')] = converter
+    assert "'an other converter already exist for .bam -> .bed'" == str(err.value)
