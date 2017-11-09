3f46f43606bdb00e7bd57f5fb2906f107d0ae84f
Thomas Cokelaer <cokelaer@gmail.com>
2017-11-09 22:29:00 +0100

    Fix tests using correct md5

--- test/test_fastq2fasta.py
+++ test/test_fastq2fasta.py
@@ -7,6 +7,11 @@ from easydev import TempFile, md5
 def test_conv():
     infile = bioconvert_data("test_fastq2fasta_v1.fastq")
 
+    # this md5sum contains @ instead of > for the identifiers
+    # outfile = bioconvert_data("test_fastq2fasta_v1.fasta")
+    # so for now, we use the md5sum (that would use > signs)
+    md5out = "27e2e0ae66fe2e2fb17c6715cafffdf0"
+
     # One temporary file for the fasta created using the method
     # and one for an unwrapped version.
     # Some methods may output multi-line fasta, so we need to
@@ -17,5 +22,7 @@ def test_conv():
             convert = Fastq2Fasta(infile, outfile.name)
             convert()
             Fastq2Fasta.unwrap_fasta(outfile.name, unwrapped.name)
-            assert md5(unwrapped.name) == md5(infile), \
+            assert md5(unwrapped.name) == md5out, \
                 "{} failed".format(method)
+
+
