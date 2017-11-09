29cb17efccb04862f9a5cce94f161cd1ef86f614
aperrin <amandine.perrin@pasteur.fr>
2017-11-09 18:23:57 +0100

    Temporary change on gz2dsrc tests
    As result dsrc file depends on the DSRC soft version, we cannot compare them if they were not created by the same version. For now, we uncompress the created dsrc file, and compare the obtained fq to the reference one.

--- test/test_gz2dsrc.py
+++ test/test_gz2dsrc.py
@@ -1,4 +1,5 @@
-import pytest
+import os
+import subprocess
 from bioconvert.gz2dsrc import GZ2DSRC
 from easydev import TempFile, md5
 
@@ -9,10 +10,20 @@ def test_gz2dsrc():
     """
     from bioconvert import bioconvert_data
     in_gz = bioconvert_data("SP1.fq.gz")
-    exp_dsrc = bioconvert_data("SP1.dsrc")
+    exp_fq = bioconvert_data("SP1.fq")
     with TempFile(suffix=".dsrc") as tempfile:
         converter = GZ2DSRC(in_gz, tempfile.name)
         converter()
 
+        # uncompress the created dsrc file, and compare uncompressed file
+        # to the expected one. We do not directly compare dsrc files as
+        # it depends on the dsrc version used...
+        # TO BE CHANGED...
+        assert os.path.isfile(tempfile.name)
+        tmp_fq = tempfile.name + ".fq"
+        cmd = "dsrc d {} {}".format(tempfile.name, tmp_fq)
+        subprocess.call(cmd.split())
+        # self.execute(cmd.format(tempfile.name, tmp_fq))
+
         # Check that the output is correct with a checksum
-        assert md5(tempfile.name) == md5(exp_dsrc)
+        assert md5(tmp_fq) == md5(exp_fq)
