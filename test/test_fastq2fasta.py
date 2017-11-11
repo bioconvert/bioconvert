7ca893a36bfe22187f839e3ac4404c968a5f4421
Thomas Cokelaer <cokelaer@gmail.com>
2017-11-11 21:27:19 +0100

    add test for bam2cram and vice-versa

--- test/test_fastq2fasta.py
+++ test/test_fastq2fasta.py
@@ -1,10 +1,12 @@
 from bioconvert.fastq2fasta import Fastq2Fasta
 from bioconvert import bioconvert_data
 from easydev import TempFile, md5
-
+import pytest
 
 # TODO: Add test of the unwrap_fasta method
-def test_conv():
+
+@pytest.mark.parametrize("method", Fastq2Fasta.available_methods)
+def test_conv(method):
     infile = bioconvert_data("test_fastq2fasta_v1.fastq")
 
     expected_outfile = bioconvert_data("test_fastq2fasta_v1.fasta")
@@ -14,11 +16,10 @@ def test_conv():
     # and one for an unwrapped version.
     # Some methods may output multi-line fasta, so we need to
     # compare md5 sums of unwrapped versions.
-    for method in Fastq2Fasta.available_methods:
-        with TempFile(suffix=".fasta") as outfile, \
-                TempFile(suffix=".fasta") as unwrapped:
-            convert = Fastq2Fasta(infile, outfile.name)
-            convert(method=method)
-            Fastq2Fasta.unwrap_fasta(outfile.name, unwrapped.name)
-            assert md5(unwrapped.name) == md5out, \
-                "{} failed".format(method)
+    with TempFile(suffix=".fasta") as outfile, \
+            TempFile(suffix=".fasta") as unwrapped:
+        convert = Fastq2Fasta(infile, outfile.name)
+        convert(method=method)
+        Fastq2Fasta.unwrap_fasta(outfile.name, unwrapped.name)
+        assert md5(unwrapped.name) == md5out, \
+            "{} failed".format(method)
