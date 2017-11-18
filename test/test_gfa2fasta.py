622cb90f60a45400a2f1a6e913a7befa3e4e32cf
Thomas Cokelaer <cokelaer@gmail.com>
2017-11-18 14:41:48 +0100

    Fix md5 of the test

--- test/test_gfa2fasta.py
+++ test/test_gfa2fasta.py
@@ -11,9 +11,11 @@ def test_conv_1(method):
         convert()
 
         # Check that the output is correct with a checksum
-        # Note that we cannot test the md5 on a gzip file but only 
-        # on the original data. 
-        assert md5(tempfile.name) == "6a02cf3308bf8d177a7d4cc55f317782"
+        # Note that awk wrap fasta while python method does not 
+        assert md5(tempfile.name) in \
+            [ "6a02cf3308bf8d177a7d4cc55f317782",
+              'aced24d965c5420781ad0884a56ed3c5']
+
 
 
 
@@ -23,10 +25,6 @@ def test_conv(method):
     expected_outfile = bioconvert_data("test_gfa2fasta.fasta")
     md5out = md5(expected_outfile)
 
-    # One temporary file for the fasta created using the method
-    # and one for an unwrapped version.
-    # Some methods may output multi-line fasta, so we need to
-    # compare md5 sums of unwrapped versions.
     with TempFile(suffix=".fasta") as outfile:
         convert = GFA2FASTA(infile, outfile.name)
         convert(method=method)
