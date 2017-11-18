88dc643c8fe87b3228940310fd4eebb07265dad0
Thomas Cokelaer <cokelaer@gmail.com>
2017-11-18 12:26:17 +0100

    update test for gfa2fasta converter

--- test/test_gfa2fasta.py
+++ test/test_gfa2fasta.py
@@ -1,10 +1,11 @@
 from bioconvert.gfa2fasta import GFA2FASTA
 from bioconvert import bioconvert_data
 from easydev import TempFile, md5
+import pytest
 
-
-def test_conv():
-    infile = bioconvert_data("test_v1.gfa")
+@pytest.mark.parametrize("method", GFA2FASTA.available_methods)
+def test_conv_1(method):
+    infile = bioconvert_data("test_gfa2fasta_v1.gfa")
     with TempFile(suffix=".fasta") as tempfile:
         convert = GFA2FASTA(infile, tempfile.name)
         convert()
@@ -13,3 +14,21 @@ def test_conv():
         # Note that we cannot test the md5 on a gzip file but only 
         # on the original data. 
         assert md5(tempfile.name) == "6a02cf3308bf8d177a7d4cc55f317782"
+
+
+
+@pytest.mark.parametrize("method", GFA2FASTA.available_methods)
+def test_conv(method):
+    infile = bioconvert_data("test_gfa2fasta.gfa")
+    expected_outfile = bioconvert_data("test_gfa2fasta.fasta")
+    md5out = md5(expected_outfile)
+
+    # One temporary file for the fasta created using the method
+    # and one for an unwrapped version.
+    # Some methods may output multi-line fasta, so we need to
+    # compare md5 sums of unwrapped versions.
+    with TempFile(suffix=".fasta") as outfile:
+        convert = GFA2FASTA(infile, outfile.name)
+        convert(method=method)
+        assert md5(convert.outfile) == md5out, \
+            "{} failed".format(method)
