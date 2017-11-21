b163f682ed48d9e1f71a85988ebe45155a4d58b5
Thomas Cokelaer <cokelaer@gmail.com>
2017-11-22 00:55:37 +0100

    download singularity if requested

--- bioconvert/core/graph.py
+++ bioconvert/core/graph.py
@@ -12,31 +12,95 @@
 #
 ##############################################################################
 """ description """
+import os
 import json
 import glob
 from os.path import join, basename
 
 
-def create_graph(filename, layout="dot"):
+def create_graph(filename, layout="dot", use_singularity=False):
     """
 
-    :param filename: should end in .png or .svg
+    :param filename: should end in .png or .svg or .dot
+
+    If extension is .dot, only the dot file is created.
+    This is useful if you have issues installing graphviz.
+    If so, under Linux you could use our singularity container
+    see github.com/cokelaer/graphviz4all
+
     """
     from bioconvert.core.registry import Registry
     rr = Registry()
 
     try:
+        if filename.endswith(".dot") or use_singularity is True:
+            raise
         from pygraphviz import AGraph
+        dg = AGraph(directed=True)
+
+        for a, b in rr.get_conversions():
+            dg.add_edge(a, b)
+
+        dg.layout(layout)
+        dg.draw(filename)
+
     except:
-        print("Please install graphviz/pygraphviz")
-        class AGraph():
-            pass
 
-    dg = AGraph(directed=True)
+        dot = """
+strict digraph{
+    node [label="\\N"];
 
-    for a, b in rr.get_conversions():
-        dg.add_edge(a, b)
+    """
+        nodes =  set([item for items in rr.get_conversions() for item in items])
 
-    dg.layout(layout)
-    dg.draw(filename)
+        for node in nodes:
+            dot += "{};\n".format(node)
+        for a, b in rr.get_conversions():
+            dot+="{} -> {};\n".format(a, b)
+        dot += "}\n"
+
+        from easydev import TempFile
+        from bioconvert import shell
+        dotfile = TempFile(suffix=".dot")
+        with open(dotfile.name, "w") as fout:
+            fout.write(dot)
+
+        dotpath = ""
+        if use_singularity:
+            # download singularity
+            from bioconvert import configuration as config
+            from easydev import md5
+            singfile = "{}/graphviz.img".format(config.user_config_dir)
+
+            print(singfile)
+
+            if os.path.exists(singfile) and md5(singfile) == "4288088d91c848e5e3a327282a1ab3d1":
+                print("Found singularity (graphviz) image")
+            else:
+                print("Downloading singularity. Please wait")
+                cmd = "singularity pull {} shub://cokelaer/graphviz4all:v1"
+                cmd = cmd.format(singfile)
+                shell(cmd)
+
+            dotpath = "singularity run {} ".format(singfile)
+
+        ext = filename.rsplit(".", 1)[1]
+        cmd = "{}dot -T{} {} -o {}".format(dotpath, ext, dotfile.name, filename)
+        shell(cmd)
+        dotfile.delete()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
