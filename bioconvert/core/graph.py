f768a46c0ccab9d59c1131eda6caa179fd06e4db
Thomas Cokelaer <cokelaer@gmail.com>
2017-11-21 21:25:10 +0100

    Remove graphviz from requirements

--- bioconvert/core/graph.py
+++ bioconvert/core/graph.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
+#
+#  This file is part of Bioconvert software
+#
+#  Copyright (c) 2017 - Bioconvert Development Team
+#
+#  Distributed under the terms of the 3-clause BSD license.
+#  The full license is in the LICENSE file, distributed with this software.
+#
+#  website: https://github.com/biokit/bioconvert
+#  documentation: http://bioconvert.readthedocs.io
+#
+##############################################################################
+""" description """
 import json
 import glob
 from os.path import join, basename
-# install this with "conda install -c conda-forge python-graphviz"
-import pygraphviz as pgv
-
 
 
 def create_graph(filename, layout="dot"):
@@ -14,7 +25,14 @@ def create_graph(filename, layout="dot"):
     from bioconvert.core.registry import Registry
     rr = Registry()
 
-    dg = pgv.AGraph(directed=True)
+    try:
+        from pygraphviz import AGraph
+    except:
+        print("Please install graphviz/pygraphviz")
+        class AGraph():
+            pass
+
+    dg = AGraph(directed=True)
 
     for a, b in rr.get_conversions():
         dg.add_edge(a, b)
