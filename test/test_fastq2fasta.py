0c21ca1c773ab7bfa6d61ef7eb962a2b47062dff
Blaise Li <blaise.li__git@nsup.org>
2017-12-19 17:37:37 +0100

    More tests for fastq2fasta.

--- test/test_fastq2fasta.py
+++ test/test_fastq2fasta.py
@@ -27,3 +27,27 @@ def test_conv(method):
             outfile.name, unwrapped.name, strip_comment=True)
         assert md5(unwrapped.name) == md5out, \
             "{} failed".format(method)
+
+@pytest.mark.parametrize("method", Fastq2Fasta.available_methods)
+def test_more_samples(method):
+    for sample_name in ["sample_v2", "sample_v3", "sample_v4"]:
+        infile = bioconvert_data("{}.fastq".format(sample_name))
+
+        expected_outfile = bioconvert_data("{}.fasta".format(sample_name))
+        with TempFile(suffix=".fasta") as expected_unwrapped:
+            Fastq2Fasta.unwrap_fasta(
+                expected_outfile, expected_unwrapped.name, strip_comment=True)
+            md5out = md5(expected_unwrapped.name)
+
+        # One temporary file for the fasta created using the method
+        # and one for an unwrapped version.
+        # Some methods may output multi-line fasta, so we need to
+        # compare md5 sums of unwrapped versions.
+        with TempFile(suffix=".fasta") as outfile, \
+                TempFile(suffix=".fasta") as unwrapped:
+            convert = Fastq2Fasta(infile, outfile.name)
+            convert(method=method)
+            Fastq2Fasta.unwrap_fasta(
+                outfile.name, unwrapped.name, strip_comment=True)
+            assert md5(unwrapped.name) == md5out, \
+                "{} failed for {}".format(method, sample_name)
