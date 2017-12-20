f25ad42a5b076fa34d8f3b52f1e080ebfcf42cf4
Thomas Cokelaer <cokelaer@gmail.com>
2017-12-20 11:23:55 +0100

    Fix test (simplify and add missing pytest decorator

--- test/test_bam2bigwig.py
+++ test/test_bam2bigwig.py
@@ -3,19 +3,21 @@ from bioconvert import bioconvert_data
 from easydev import TempFile, md5
 import pytest
 
-def test_conv(method):
-    infile = bioconvert_data('test_measles.sorted.bam')
-    
-    expected_outputfile = bioconvert_data('test_measles.bigwig')
-    with TempFile(suffix='.bigwig') as expected_unwrapped:
-        BAM2BIGWIG.unwrap_bigwig(expected_outfile, expected_unwrapped.name, strip_comment = True)
-        md5out= md5(expected_unwrapped.name)
 
-    with TempFile(suffix=".bigwig") as outfile, \
-            TempFile(suffix=".bigwig") as unwrapped:
+# Here we will scan all available methods and repeat the test
+# automatically for each method
+@pytest.mark.parametrize("method", BAM2BIGWIG.available_methods)
+def test_conv(method):
+
+    # the input file
+    infile = bioconvert_data('test_measles.sorted.bam')
+
+    # What is the expected md5sum of the final output file ?
+    expected_outputfile = bioconvert_data('test_measles.bigwig')
+    md5out= md5(expected_outputfile)
+
+    # Call convert and check that the output file created has the correct md5sum
+    with TempFile(suffix=".bigwig") as outfile:
         convert = BAM2BIGWIG(infile, outfile.name)
         convert(method=method)
-        BAM2BIGWIG.unwrap_fasta(
-            outfile.name, unwrapped.name, strip_comment=True)
-        assert md5(unwrapped.name) == md5out, \
-            "{} failed".format(method)
+        assert md5(outfile.name) == md5out, "{} failed".format(method)
