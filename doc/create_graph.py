8799121d93df49fcfbc013e270baa3e90e9930fc
Thomas Cokelaer <cokelaer@gmail.com>
2018-01-04 20:01:22 +0100

    add file to create summary image

---
+++ doc/create_graph.py
@@ -0,0 +1,9 @@
+from bioconvert.core.graph import create_graph
+
+
+#####################################################
+# Get a data set (BAM file) for testing
+import matplotlib as mpl
+mpl.rcParams['figure.dpi']= 250
+create_graph("conversion.png")
+
