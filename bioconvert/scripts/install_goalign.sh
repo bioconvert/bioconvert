62f08f3fdcb07f5aa62a8c4b7f837e381b5b0cf8
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-01-12 10:20:27 +0100

    Corrected install scripts paths

--- bioconvert/scripts/install_goalign.sh
+++ bioconvert/scripts/install_goalign.sh
@@ -1,5 +1,7 @@
+[ -z "$GOPATH" ] && GOPATH="$HOME/go/"
+PATH=$GOPATH/bin:$PATH
 go get -u github.com/golang/dep/cmd/dep
 go get github.com/fredericlemoine/goalign/
 cd $GOPATH/src/github.com/fredericlemoine/goalign/
-$GOPATH/bin/dep ensure
+dep ensure
 make
--- bioconvert/scripts/install_gotree.sh
+++ bioconvert/scripts/install_gotree.sh
@@ -1,5 +1,7 @@
+[ -z "$GOPATH" ] && GOPATH="$HOME/go/"
+PATH=$GOPATH/bin:$PATH
 go get -u github.com/golang/dep/cmd/dep
 go get github.com/fredericlemoine/gotree/
 cd $GOPATH/src/github.com/fredericlemoine/gotree/
-$GOPATH/bin/dep ensure
+dep ensure
 make
