e72f64906836241476e357925c946da3ee6e8f71
Thomas Cokelaer <cokelaer@gmail.com>
2018-01-14 16:37:22 +0100

    Fix test (phylip2fasta fix) and add more (embl2genbank)

--- test/test_phylip2fasta.py
+++ test/test_phylip2fasta.py
@@ -1,5 +1,4 @@
 import os
-import pytest
 from easydev import TempFile, md5
 
 from bioconvert import bioconvert_data
@@ -7,20 +6,23 @@ from bioconvert.phylip2fasta import PHYLIP2FASTA
 import pytest
 
 
-skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ
-                                and os.environ['TRAVIS_PYTHON_VERSION'].startswith("2"), reason="On travis")
+#skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ
+#                                and os.environ['TRAVIS_PYTHON_VERSION'].startswith("2"), reason="On travis")
 
 
-@skiptravis
+#@skiptravis
 @pytest.mark.parametrize("method", PHYLIP2FASTA.available_methods)
 def test_fa2phy_biopython(method):
     infile = bioconvert_data("biopython.phylip")
     outfile = bioconvert_data("biopython.fasta")
     with TempFile(suffix=".phylip") as tempfile:
         converter = PHYLIP2FASTA(infile, tempfile.name)
-        converter(method='biopython')
+        converter(method=method)
 
         # Check that the output is correct with a checksum
-        assert md5(tempfile.name) == md5(outfile)
+        if method in ["squizz", "goalign"] :
+            assert md5(tempfile.name) == "98b661ab0f22ab6d4172135686cfc363"
+        else:
+            assert md5(tempfile.name) == md5(outfile)
 
 
