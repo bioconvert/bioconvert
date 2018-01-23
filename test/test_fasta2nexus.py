21bc043e137416ee7c95ea94b21867a64cf7b4a8
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-01-23 19:23:13 +0100

    Reformatting

--- test/test_fasta2nexus.py
+++ test/test_fasta2nexus.py
@@ -1,16 +1,14 @@
-import os
 import pytest
 from easydev import TempFile, md5
 
 from bioconvert import bioconvert_data
 from bioconvert.fasta2nexus import FASTA2NEXUS
-import pytest
 
 
 @pytest.mark.parametrize("method", FASTA2NEXUS.available_methods)
 def test_fa2nx_biopython(method):
-    infile = bioconvert_data(method+".fasta")
-    outfile = bioconvert_data(method+".nexus")
+    infile = bioconvert_data(method + ".fasta")
+    outfile = bioconvert_data(method + ".nexus")
     with TempFile(suffix=".nx") as tempfile:
         converter = FASTA2NEXUS(infile, tempfile.name)
         converter(method=method)
--- test/test_phylip2fasta.py
+++ test/test_phylip2fasta.py
@@ -1,9 +1,8 @@
-import os
+import pytest
 from easydev import TempFile, md5
 
 from bioconvert import bioconvert_data
 from bioconvert.phylip2fasta import PHYLIP2FASTA
-import pytest
 
 
 @pytest.mark.parametrize("method", PHYLIP2FASTA.available_methods)
@@ -15,9 +14,7 @@ def test_fa2phy_biopython(method):
         converter(method=method)
 
         # Check that the output is correct with a checksum
-        if method in ["squizz", "goalign"] :
+        if method in ["squizz", "goalign"]:
             assert md5(tempfile.name) == "98b661ab0f22ab6d4172135686cfc363"
         else:
             assert md5(tempfile.name) == md5(outfile)
-
-
