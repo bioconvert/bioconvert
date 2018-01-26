b1ccc66c4d5e0439d3144f20cbd72e2c35e60111
Frederic Lemoine <fredericlemoine@users.noreply.github.com>
2018-01-26 14:51:52 +0100

    Added Genbank2GFF3

--- bioconvert/core/__init__.py
+++ bioconvert/core/__init__.py
@@ -1,6 +1,5 @@
 
 
-
 from easydev import AttrDict
 
 
@@ -14,13 +13,14 @@ extensions = {
     'bz2': ['bz2'],
     'cdao': ["cdao"],                           # phylo
     'cram': ["cram"],                           # alignment
-    'clustal':["clustal", "aln", "clw"],        # phylo
+    'clustal': ["clustal", "aln", "clw"],       # phylo
     'dsrc': ['dsrc'],
     'embl': ['embl'],                           # annotation/sequence
     'fasta': ["fasta", "fa", "fst"],            # sequence
     'fastq': ["fastq", "fq"],                   # sequence
     'genbank': ['genbank', 'gbk', "gb"],        # annotation/sequence
     'gfa': ['gfa'],                             # assembly
+    'gff': ['gff3', 'gff'],                     # gff3 so far
     'gz': ['gz'],
     'json': ['json'],                           # misc
     'newick': ["newick", "nw", "nhx", "nwk"],   # phylo
@@ -34,7 +34,7 @@ extensions = {
     'vcf': ['vcf'],                             # variant
     'twobit': ['2bit'],                         # sequence
     'yaml': ['yaml', 'YAML'],                   # misc
-    }
+}
 
 extensions = AttrDict(**extensions)
 
@@ -67,31 +67,31 @@ and .fastq, are commonly used. There are different FASTQ formats for different
 sequencing technologies.
 
 ig  Unspecified (*.txt)     1.47    No  1.52    This refers to the
-IntelliGenetics file format, apparently the same as the MASE alignment format.  
+IntelliGenetics file format, apparently the same as the MASE alignment format.
 
 imgt    Unspecified (*.txt)     1.56    1.56    1.56    This refers to the IMGT
-variant of the EMBL plain text file format.     
+variant of the EMBL plain text file format.
 
 
-phd     *.phd   1.46    1.52    1.52    PHD files are output from PHRED, used by
-PHRAP and CONSED for input. Uses Bio.Sequencing.Phd internally.     
+phd     *.phd   1.46    1.52    1.52    PHD files are output from PHRED,
+used by PHRAP and CONSED for input. Uses Bio.Sequencing.Phd internally.
 
 pir     *.pir   1.48    No  1.52    A "FASTA like" format introduced by the
 National Biomedical Research Foundation (NBRF) for the Protein Information
-Resource (PIR) database, now part of UniProt.   
+Resource (PIR) database, now part of UniProt.
 
 sff     *.sff   1.54    1.54    1.54    Standard Flowgram Format (SFF) binary
-files produced by Roche 454 and IonTorrent/IonProton sequencing machines.   
+files produced by Roche 454 and IonTorrent/IonProton sequencing machines.
 
 swiss   *.sw    1.43    No  1.52    Swiss-Prot aka UniProt format. Uses
-Bio.SwissProt internally. See also the UniProt XML format.  
+Bio.SwissProt internally. See also the UniProt XML format.
 
 qual    *.qual  1.50    1.50    1.52    Qual files are a bit like FASTA files
-but instead of the sequence, record space separated integer sequencing values as
-PHRED quality scores. A matched pair of FASTA and QUAL files are often used as
-an alternative to a single FASTQ file.  
+but instead of the sequence, record space separated integer sequencing values
+as PHRED quality scores. A matched pair of FASTA and QUAL files are often used
+as an alternative to a single FASTQ file.
 
-uniprot-xml     *.xml   1.56    No  1.56    UniProt XML format, successor to the
-plain text Swiss-Prot format.
+uniprot-xml     *.xml   1.56    No  1.56    UniProt XML format, successor to
+the plain text Swiss-Prot format.
 
 """
---
+++ bioconvert/genbank2gff.py
@@ -0,0 +1,52 @@
+
+
+# -*- coding: utf-8 -*-
+##############################################################################
+#  This file is part of Bioconvert software
+#
+#  Copyright (c) 2017 - Bioconvert Development Team
+#
+#  Distributed under the terms of the 3-clause BSD license.
+#  The full license is in the LICENSE file, distributed with this software.
+#
+#  website: https://github.com/biokit/bioconvert
+#  documentation: http://bioconvert.readthedocs.io
+##############################################################################
+""" Converts Genbank format to GFF3 using biocode
+https://github.com/jorvis/biocode/
+
+We may want to do it directly in python in the future,
+without calling the external script
+"""
+
+from bioconvert import ConvBase
+
+__all__ = ["GENBANK2GFF"]
+
+
+class GENBANK2GFF(ConvBase):
+    """Convert :term:`GENBANK` file to :term:`EMBL` file
+
+    Some description.
+
+    """
+
+    def __init__(self, infile, outfile, *args, **kargs):
+        """.. rubric:: constructor
+
+        :param str infile: input GENBANK file
+        :param str outfile: output EMBL filename
+
+        """
+        super(GENBANK2GFF, self).__init__(infile, outfile, *args, **kargs)
+
+        self._default_method = "python"
+
+    def _method_biocode(self, *args, **kwargs):
+        """Uses scripts from biocode
+        See: https://github.com/jorvis/biocode/
+        https://github.com/jorvis/biocode/blob/master/gff/convert_genbank_to_gff3.py
+        """
+        cmd = "convert_genbank_to_gff3.py -i {} -o {} --no_fasta".format(
+            self.infile, self.outfile)
+        self.execute(cmd)
