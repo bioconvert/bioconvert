5f030c3f3f28267d188f2b62423dce09e1f635cd
Nicolas <ntraut9@gmail.com>
2018-02-20 17:18:41 +0100

    add plink ped+map <-> bed+bim+fam converters

---
+++ bioconvert/plink2bplink.py
@@ -0,0 +1,34 @@
+import colorlog
+
+from bioconvert import ConvBase
+
+_log = colorlog.getLogger(__name__)
+
+
+class PLINK2BPLINK(ConvBase):
+    """Converts a genotype dataset ped+map in :term:`PLINK` format to
+    bed+bim+fam :term:`BPLINK` format
+
+    Conversion is based on plink executable
+
+    """
+
+    def __init__(self, infile, outfile=None, *args, **kwargs):
+        """.. rubric:: constructor
+
+        :param str infile: input :term:`PLINK` file.
+        :param str outfile: (optional) output :term:`BPLINK` file
+        """
+        if not outfile:
+            outfile = generate_outfile_name(infile, '')
+        super().__init__(infile, outfile)
+        self._default_method = 'plink'
+
+    def _method_plink(self, *args, **kwargs):
+        """
+        Convert plink file in text using plink executable.
+        """
+        cmd = 'plink --file {infile} --make-bed --out {outfile}'.format(
+            infile=self.infile,
+            outfile=self.outfile)
+        self.execute(cmd)
