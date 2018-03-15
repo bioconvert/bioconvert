74d14a5f9d49fc44e8d19bd435aa208fd162c20f
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2018-03-15 18:23:17 +0100

    Bugfixe in csv2tsv test : wrong filename

--- test/test_csv2tsv.py
+++ test/test_csv2tsv.py
@@ -9,8 +9,8 @@ from bioconvert.csv2tsv import CSV2TSV
 
 @pytest.mark.parametrize("method", CSV2TSV.available_methods)
 def test_conv(method):
-    infile = bioconvert_data("test_v1.csv")
-    expected_outile = bioconvert_data("test_v1.tsv")
+    infile = bioconvert_data("test_csv2tsv.csv")
+    expected_outile = bioconvert_data("test_csv2tsv.tsv")
     with TempFile(suffix=".tsv") as tempfile:
         convert = CSV2TSV(infile, tempfile.name)
         convert()
--- test/test_tsv2csv.py
+++ test/test_tsv2csv.py
@@ -9,8 +9,8 @@ from bioconvert.tsv2csv import TSV2CSV
 
 @pytest.mark.parametrize("method", TSV2CSV.available_methods)
 def test_conv(method):
-    infile = bioconvert_data("test_v1.tsv")
-    expected_outile = bioconvert_data("test_v1.csv")
+    infile = bioconvert_data("test_csv2tsv.tsv")
+    expected_outile = bioconvert_data("test_csv2tsv.csv")
     with TempFile(suffix=".csv") as tempfile:
         convert = TSV2CSV(infile, tempfile.name)
         convert()
