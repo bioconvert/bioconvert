b944e9f7cb68498f36cbc9246f1fa88ba1bbb807
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2018-03-19 10:08:46 +0100

    fixing python lib name reducing scope of test_bam2fastq.py

--- test/test_bam2fastq.py
+++ test/test_bam2fastq.py
@@ -1,14 +1,12 @@
 from bioconvert.bam2fastq import BAM2Fastq
 from bioconvert import bioconvert_data
 from easydev import TempFile, md5
-import pytest
 
-@pytest.mark.parametrize("method", BAM2Fastq.available_methods)
-def test_conv(method):
+def test_conv():
     infile = bioconvert_data("test_measles.sorted.bam")
     with TempFile(suffix=".fq") as tempfile:
         convert = BAM2Fastq(infile, tempfile.name)
-        convert(method=method)
+        convert()
 
         # Check that the output is correct with a checksum
         # Note that we cannot test the md5 on a gzip file but only 
@@ -17,5 +15,5 @@ def test_conv(method):
         assert md5(tempfile.name) == "8683ad696e52e4af67670d2631af6d1f"
 
 
-        # for method in convert.available_methods:
-        #     convert(method=method)
+        for method in convert.available_methods:
+            convert(method=method)
