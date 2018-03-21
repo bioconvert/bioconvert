adcb679b7facbff131abeb967a39c8b50b8f0174
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2018-03-21 11:57:51 +0100

    typo in input/output file

--- test/test_bam2cram.py
+++ test/test_bam2cram.py
@@ -8,8 +8,8 @@ import pytest
 
 @pytest.mark.parametrize("method", BAM2CRAM.available_methods)
 def test_conv(method):
-    infile = bioconvert_data("test_measles.cram")
-    outfile = bioconvert_data("test_measles.bam")
+    infile = bioconvert_data("test_measles.bam")
+    outfile = bioconvert_data("test_measles.cram")
     #reference = bioconvert_data("test_measles.fa")
 
     with TempFile(suffix=".cram") as tempfile:
