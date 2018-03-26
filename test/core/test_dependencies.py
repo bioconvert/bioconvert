3b60b454da53ac8293c7cd5013d785ddbaec7f0b
Bryan Brancotte <bryan.brancotte@pasteur.fr>
2018-03-26 16:51:36 +0200

    prevent to test plink on travis

--- test/core/test_dependencies.py
+++ test/core/test_dependencies.py
@@ -4,8 +4,7 @@ import os
 
 from bioconvert.core.decorators import requires, get_known_dependencies_with_availability
 
-skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ and
-                                os.environ['TRAVIS_PYTHON_VERSION'].startswith("2"), reason="On travis")
+skiptravis = pytest.mark.skipif("TRAVIS_PYTHON_VERSION" in os.environ, reason="On travis")
 
 
 ### AUTOMATICALLY GENERATED TESTS (START)
