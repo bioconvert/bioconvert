25035b7aeb767ab01e98e9b91f6693dfdb691ecb
Thomas Cokelaer <cokelaer@gmail.com>
2018-04-04 00:24:13 +0200

    explicit paramter

--- bioconvert/embl2genbank.py
+++ bioconvert/embl2genbank.py
@@ -30,8 +30,7 @@ class EMBL2GENBANK(ConvBase):
         """
         super(EMBL2GENBANK, self).__init__(infile, outfile, *args, **kargs)
 
-
-    @requires("squizz")
+    @requires(external_binary="squizz")
     def _method_squizz(self, *args, **kwargs):
         """Header is less informative than the one obtained with biopython"""
         cmd = "squizz {} -f embl -c genbank > {} ".format(self.infile, self.outfile)
