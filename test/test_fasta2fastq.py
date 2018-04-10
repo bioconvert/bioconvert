c54737ab3be5e8237c7d68e5f7e2504ca4baf7ef
Etienne Kornobis <etienne.kornobis@pasteur.fr>
2018-04-10 17:30:34 +0200

    Adding test for fasta2fastq with quality file

--- test/test_fasta2fastq.py
+++ test/test_fasta2fastq.py
@@ -8,9 +8,13 @@ import pytest
 @pytest.mark.parametrize("method", Fasta2Fastq.available_methods)
 def test_conv(method):
     infile = bioconvert_data("test_fastq2fasta_v1.fasta")
+    qual_file = bioconvert_data("test_fasta2fastq.qual")
 
-    expected_outfile = bioconvert_data("test_fasta2fastq.fastq")
-    md5out = md5(expected_outfile)
+    expected_outfile_no_qual = bioconvert_data("test_fasta2fastq.fastq")
+    md5out_no_qual = md5(expected_outfile_no_qual)
+
+    expected_outfile_qual = bioconvert_data("test_fastq2fasta_v1.fastq")
+    md5out_qual = md5(expected_outfile_qual)    
 
     # One temporary file for the fasta created using the method
     # and one for an unwrapped version.
@@ -19,5 +23,13 @@ def test_conv(method):
     with TempFile(suffix=".fastq") as outfile:
         convert = Fasta2Fastq(infile, outfile.name)
         convert(method=method)
-        assert md5(outfile.name) == md5out, \
+        assert md5(outfile.name) == md5out_no_qual, \
             "{} failed".format(method)
+
+    with TempFile(suffix=".fastq") as outfile:
+        convert = Fasta2Fastq(infile, outfile.name)
+        convert(method=method, quality_file=qual_file)
+        assert md5(outfile.name) == md5out_qual, \
+            "{} failed".format(method)
+
+        
