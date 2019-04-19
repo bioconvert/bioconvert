3894fc19b98788ac6bd26b2ac49f0b493a86a0f6
Thomas Cokelaer <cokelaer@gmail.com>
2019-04-19 12:37:16 +0200

    fix test

--- test/core/test_benchmark.py
+++ test/core/test_benchmark.py
@@ -8,8 +8,14 @@ from bioconvert.bam2bed import BAM2BED
 from easydev import TempFile
 
 
+def is_osx():
+    if "TRAVIS_OS_NAME" in os.environ:
+        if os.environ["TRAVIS_OS_NAME"] == "osx":
+            return True
+    return False
+
 @pytest.mark.skipif("DISPLAY" not in os.environ, reason="no DISPLAY available, will fail otherwise")
-@pytest.mark.skipif("TRAVIS_OS_NAME" == "osx", reason="unknown failure on travis april 2019")
+@pytest.mark.skipif(is_osx(), reason="unknown failure on travis april 2019")
 def test_benchmark():
     input_file = bioconvert_data("test_measles.sorted.bam")
     with TempFile(suffix=".bed") as fout:
