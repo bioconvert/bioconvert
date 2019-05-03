45a3c6d9305b0a4cb76f40b534a0a8a371366bf1
Thomas Cokelaer <cokelaer@gmail.com>
2019-05-03 17:00:41 +0200

    Fix test (comment/uncomment)

--- test/test_phylip2xmfa.py
+++ test/test_phylip2xmfa.py
@@ -7,16 +7,12 @@ from bioconvert.phylip2xmfa import PHYLIP2XMFA
 
 @pytest.mark.parametrize("method", PHYLIP2XMFA.available_methods)
 def test_phy2xmfa(method):
-    if method == "goalign":
-        # FIXME: https://github.com/evolbioinfo/goalign/issues/3
-        pass
-    else:
-        infile = bioconvert_data(method + ".phylip")
-        outfile = bioconvert_data("test_phylip2xmfa.xmfa")
-        with TempFile(suffix=".xmfa") as tempfile:
-            converter = PHYLIP2XMFA(infile, tempfile.name)
-            converter(method=method)
+    infile = bioconvert_data(method + ".phylip")
+    outfile = bioconvert_data("test_phylip2xmfa.xmfa")
+    with TempFile(suffix=".xmfa") as tempfile:
+        converter = PHYLIP2XMFA(infile, tempfile.name)
+        converter(method=method)
 
-            # Check that the output is correct with a checksum
-            #TODO
-            #assert md5(tempfile.name) == md5(outfile)
+        # Check that the output is correct with a checksum
+        #TODO
+        #assert md5(tempfile.name) == md5(outfile)
