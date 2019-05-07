c39b4aa01d63501182d44ebd9cb7757ffda07876
Thomas Cokelaer <cokelaer@gmail.com>
2019-05-07 10:53:51 +0200

    add test for maf tools

---
+++ test/utils/test_maf.py
@@ -0,0 +1,19 @@
+from bioconvert.utils.maf import MAF
+from bioconvert.utils import maf
+from easydev import TempFile
+
+from bioconvert import bioconvert_data
+
+
+filename = bioconvert_data("test_maf2sam.maf")
+
+
+def test_read_maf():
+
+    with TempFile(suffix=".sam") as fout:
+        maf = MAF(filename, fout.name)
+        #maf.count_insertions()
+        maf.to_sam()
+        
+def test_others():
+    maf.mapqFromProb("0.5")
