32000f87c24bc41a2aa49c15e997d2c83505b33a
Bertrand Néron <bneron@pasteur.fr>
2019-06-05 17:22:28 +0200

    :bug: fix test for nexus2fasta for goalign method

--- test/test_nexus2fasta.py
+++ test/test_nexus2fasta.py
@@ -8,8 +8,13 @@ from bioconvert.nexus2fasta import NEXUS2FASTA
 @pytest.mark.parametrize("method", NEXUS2FASTA.available_methods)
 def test_nx2fa_biopython(method):
     if method == "goalign":
-        pass
-
+        infile = bioconvert_data("goalign.nexus")
+        outfile = bioconvert_data("goalign.fasta")
+        with TempFile(suffix=".fasta") as tempfile:
+            converter = NEXUS2FASTA(infile, tempfile.name)
+            converter(method=method)
+            # Check that the output is correct with a checksum
+            assert md5(tempfile.name) == md5(outfile)
     else:
         infile = bioconvert_data("nexus2fasta_"+ method + ".nexus")
         outfile = bioconvert_data("nexus2fasta_" + method + ".fasta")
