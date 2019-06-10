40aad307502b458cf0753f88d634749442c99013
cokelaer <cokelaer@gmail.com>
2019-06-10 23:36:11 +0200

    rename file

---
+++ test/test_bedgraph2cov.py
@@ -0,0 +1,16 @@
+import pytest
+from easydev import TempFile, md5
+
+from bioconvert import bioconvert_data
+from bioconvert.bedgraph2cov import BEDGRAPH2COV
+
+
+@pytest.mark.parametrize("method", BEDGRAPH2COV.available_methods)
+def test_bedgraph2bed(method):
+    infile = bioconvert_data("test_bedgraph2bed.bedgraph")
+    with TempFile(suffix=".cov") as tempfile:
+        converter = BEDGRAPH2COV(infile, tempfile.name)
+        converter(method=method)
+
+        # Check that the output is correct with a checksum
+        assert md5(tempfile.name) == "a8cc8b0fd2f2fd028424dc8969a0b8b6"
