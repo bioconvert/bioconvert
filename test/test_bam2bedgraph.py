80b9a170cd0513b43f68901dacfac79ea89c30eb
Thomas Cokelaer <cokelaer@gmail.com>
2019-06-11 18:53:28 +0200

    Fix test

--- test/test_bam2bedgraph.py
+++ test/test_bam2bedgraph.py
@@ -10,10 +10,19 @@ def test_conv(method):
         convert = BAM2BEDGRAPH(infile, tempfile.name)
         convert(method=method)
 
-        # changes after 3.1 
+        # changes after 3.1
         assert md5(tempfile.name) == "5be280e9f74e9ff1128ff1d2fe3e0812"
 
 
+
+def is_osx():
+    if "TRAVIS_OS_NAME" in os.environ:
+        if os.environ["TRAVIS_OS_NAME"] == "osx":
+            return True
+    return False
+
+
+@pytest.mark.skipif(is_osx(), reason="no mosdepth on macx/conda")
 def test_conv_mosdepth_gz():
     infile = bioconvert_data("test_measles.sorted.bam")
     with TempFile(suffix=".bedgraph.gz") as tempfile:
