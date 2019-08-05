82ca590d5f92195d9ea4d6682b68f06058f000a7
Thomas Cokelaer <cokelaer@gmail.com>
2019-08-05 20:50:17 +0200

    update doc

--- bioconvert/bam2bigwig.py
+++ bioconvert/bam2bigwig.py
@@ -42,7 +42,10 @@ __all__ = ["BAM2BIGWIG"]
 class BAM2BIGWIG(ConvBase):
     """Convert :term:`BAM` file to :term:`BIGWIG` file
 
-    Some description.
+    Convert BAM into a binary version of :term:`WIG` format.
+
+    Methods are base on bamCoverage [BAMCOVERAGE]_ and bedGraphToBigWig from
+    wiggletools [WIGGLETOOLS]_.
 
     """
     _default_method = "bamCoverage"
@@ -53,23 +56,19 @@ class BAM2BIGWIG(ConvBase):
         :param str infile: input BAM file
         :param str outfile: output BIGWIG filename
 
-        command used::
-            bamCoverage -bam {} –-outFileFormat bigwig --outFileName {}
-
-
         """
         super(BAM2BIGWIG, self).__init__(infile, outfile, *args, **kargs)
 
     @requires("bamCoverage")
     def _method_bamCoverage(self, *args, **kwargs):
         """run bamCoverage package"""
-        cmd = "bamCoverage --bam {} --outFileFormat bigwig --outFileName {}".format(
+        cmd = "bamCoverage --bam {} --outFileFormat bigwig --outFileName {} 1>out 2>stderr".format(
                 self.infile, self.outfile)
         self.execute(cmd)
 
     @requires(external_binaries=["bedGraphToBigWig", "bedtools"])
     def _method_ucsc(self, *args, **kwargs):
-        """run bam2bigwig using bioconvert/bedtools bam2bedgraph and ucsc tool bedGraphToBigWig"""
+        """run ucsc tool bedGraphToBigWig"""
         from bioconvert.bam2bedgraph import BAM2BEDGRAPH
         from bioconvert.bedgraph2bigwig import BEDGRAPH2BIGWIG
 
@@ -87,5 +86,5 @@ class BAM2BIGWIG(ConvBase):
             names="--chrom-sizes",
             default=None,
             help="a two-column file/URL: <chromosome name> <size in bases>. "
-                 "Used by the bedtools method only",
+                 "Used by the ucsc method only",
         )
