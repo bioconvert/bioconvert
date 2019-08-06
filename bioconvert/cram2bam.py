52688851887ff573645579320852032e6b3eff74
Thomas Cokelaer <cokelaer@gmail.com>
2019-08-07 00:03:10 +0200

    update code to use --reference

--- bioconvert/cram2bam.py
+++ bioconvert/cram2bam.py
@@ -24,7 +24,7 @@
 """Convert :term:`CRAM` file to :term:`BAM` file"""
 import os
 from bioconvert import ConvBase
-from easydev.multicore import cpu_count
+from bioconvert.core.base import ConvArg
 
 import colorlog
 
@@ -37,55 +37,50 @@ class CRAM2BAM(ConvBase):
     """Convert :term:`CRAM` file to :term:`BAM` file
 
     The conversion requires the reference corresponding to the input file
-    It can be provided as an argument in the constructor. Otherwise,
-    a local file with same name as the input file but an .fa extension is
-    looked for. Otherwise, we ask for the user to provide the input file.
-    This is useful for the standalone application.
+    It can be provided as an argument with the standalone (*-\\-reference*). 
+    Otherwise, users are asked to provide it.
 
+    Methods available are based on samtools [SAMTOOLS]_.
     """
     _default_method = "samtools"
     _threading = True
 
-    def __init__(self, infile, outfile, reference=None, *args, **kargs):
+    def __init__(self, infile, outfile, *args, **kargs):
         """.. rubric:: constructor
 
         :param str infile: input CRAM file
         :param str outfile: output BAM filename
-        :param str reference: reference file in :term:`FASTA` format
 
-        command used::
-
-            samtools view -@ <thread> -Sh -T <reference> in.cram > out.bam
-
-        .. note:: the API related to the third argument may change in the future.
         """
         super(CRAM2BAM, self).__init__(infile, outfile, *args, **kargs)
 
-
-        self.reference = reference
-        if self.reference is None:
-            logger.debug("No reference provided. Infering from input file")
-            # try to find the local file replacing .sam by .fa
-            reference = infile.replace(".cram", ".fa")
-            if os.path.exists(reference):
-                logger.debug(
-                    "Reference found from inference ({})".format(reference))
-            else:
-                logger.debug("No reference found.")
-                msg = "Please enter the reference corresponding "
-                msg += "to the input CRAM file:"
-                reference = input(msg)
-                if os.path.exists(reference) is False:
-                    raise IOError("Reference required")
-                else:
-                    logger.debug("Reference exists ({}).".format(reference))
-
-            self.reference = reference
+    def _get_reference(self):
+        # In case the --reference is not used
+        msg = "Please enter the reference corresponding "
+        msg += "to the input BAM file:"
+        reference = input(msg)
+        if os.path.exists(reference) is False:
+            raise IOError("Reference required")
+        else:
+            logger.debug("Reference exists ({}).".format(reference))
+        return reference
 
     @requires("samtools")
     def _method_samtools(self, *args, **kwargs):
         # -b means output is BAM
-        reference = kwargs.get("reference", self.reference)
+        reference = kwargs.get("reference", None)
+        if reference is None:
+            reference = self._get_reference()
+
         cmd = "samtools view -@ {} -b -T {} {} > {}".format(
             self.threads, reference, self.infile, self.outfile)
         self.execute(cmd)
+
+    @classmethod
+    def get_additional_arguments(cls):
+        yield ConvArg(
+            names="--reference",
+            default=None,
+            help="the reference used (FASTA format). If not provided, prompt will appear"
+            )
+
