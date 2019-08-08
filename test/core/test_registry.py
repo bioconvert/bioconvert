147bc76416bf8007f42952b13be5e470414bd114
Thomas Cokelaer <cokelaer@gmail.com>
2019-08-09 00:55:04 +0200

    more tests

--- test/core/test_registry.py
+++ test/core/test_registry.py
@@ -26,15 +26,38 @@ def test_registry_with_dependencies():
 
 def test_registry_with_less_dependencies():
     rr = Registry()
-    # Isnt'it supposed to be upper case?
-    # assert rr.conversion_exists("bam", "bed") is True
-    # assert rr.conversion_exists("bam", "dummy") is False
     assert rr.conversion_exists(("FASTQ",), ("FASTA",))
     assert not rr.conversion_exists(("BAM",), ("DUMMY",))
     assert rr.conversion_exists(("CLUSTAL",), ("FASTQ",), allow_indirect=True)
 
+    # one to one
     assert (('FASTQ',), ('FASTA',)) in rr
+    # unexisting entry
     assert (('NIMPORT',), ('NAOIK',)) not in rr
+    # one to many
+    assert (('FASTQ',), ('FASTA', 'QUAL')) in rr
+    # without tuples
+    assert ['FASTQ', 'FASTA'] in rr
+    assert [('FASTQ',), 'FASTA'] in rr
+    assert [['FASTQ'], 'FASTA'] in rr
+    assert [['FASTQ'], ['FASTA']] in rr
+    assert ['FASTQ', ('FASTA',)] in rr
+    # argument must be a list or tuple
+    try:
+        'FASTA' in rr
+        assert False
+    except ValueError:
+        assert True
+    # must have 2 items only
+    try:
+        ['a', 'b', 'c'] in rr
+        assert False
+    except ValueError:
+        assert True
+
+    # iterator through the conversions
+    for this in rr:
+        assert this
 
     converter = rr[('FASTQ',), ('FASTA',)]
     with pytest.raises(KeyError) as err:
