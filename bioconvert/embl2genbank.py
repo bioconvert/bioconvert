a5f002471eec7da36d48e68bd605771611ba77b7
Thomas Cokelaer <cokelaer@gmail.com>
2019-08-08 16:16:02 +0200

    fix docstrings

--- bioconvert/embl2genbank.py
+++ bioconvert/embl2genbank.py
@@ -24,12 +24,19 @@
 """Convert :term:`EMBL` file to :term:`GENBANK` file"""
 from bioconvert import ConvBase
 from bioconvert.core.decorators import requires
+from bioconvert.core.decorators import compressor
+
 
 __all__ = ["EMBL2GENBANK"]
 
 
 class EMBL2GENBANK(ConvBase):
-    """Convert :term:`EMBL` file to :term:`GENBANK` file"""
+    """Convert :term:`EMBL` file to :term:`GENBANK` file
+
+
+    Methods available are based on squizz [SQUIZZ]_ and 
+    biopython [BIOPYTHON]_.
+    """
     _default_method = "biopython"
 
     def __init__(self, infile, outfile, *args, **kargs):
@@ -42,12 +49,14 @@ class EMBL2GENBANK(ConvBase):
         super(EMBL2GENBANK, self).__init__(infile, outfile, *args, **kargs)
 
     @requires(external_binary="squizz")
+    @compressor
     def _method_squizz(self, *args, **kwargs):
         """Header is less informative than the one obtained with biopython"""
         cmd = "squizz -f embl -c genbank {} > {} ".format(self.infile, self.outfile)
         self.execute(cmd)
 
     @requires(python_library="biopython")
+    @compressor
     def _method_biopython(self, *args, **kwargs):
         from Bio import SeqIO
         SeqIO.convert(self.infile, "embl", self.outfile, "genbank")
