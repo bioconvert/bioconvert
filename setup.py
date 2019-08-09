d0d17588c9ea72c8aa4a69e01bff35ee2c25f65b
Thomas Cokelaer <cokelaer@gmail.com>
2019-08-10 01:17:43 +0200

    update version to stable release

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import os
 from setuptools import setup, find_packages
 
 _MAJOR = 0
-_MINOR = 3
-_MICRO = 1
+_MINOR = 4
+_MICRO = 0
 version = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release = '%d.%d' % (_MAJOR, _MINOR)
 
