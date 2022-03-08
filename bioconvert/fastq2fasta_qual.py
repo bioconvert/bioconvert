ac55edd0acbe0d6dd99e8954cb3832f971d754e2
HugoCaro <hugo.caro@u-psud.fr>
2022-03-08 11:53:14 +0100

    Workflow fastq2fasta_qual

--- bioconvert/fastq2fasta_qual.py
+++ bioconvert/fastq2fasta_qual.py
@@ -41,6 +41,7 @@ class FASTQ2FASTA_QUAL(ConvBase):
     Method based on Python.
 
     """
+    #: Default value
     _default_method = "python"
 
     def __init__(self, infile, outfile, *args, **kargs):
